2cae671a23173d3533ce09134ff87aa1022115c0
Khanh Tran <katetrannbkhanh@gmail.com>
2024-05-25 12:07:30 -0700

    Updating

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Checkout({cart}){
     const {subtotal, tax, total} = cart
-    return(
+    
+    if (total === 0) {
+        return(
+            <div className='checkout'>
+                <h1>Order Summary</h1>
+                <p id='no-item'>Your Cart is Empty</p>
+            </div>
+        )
+    } else {
+        return(
         <div className='checkout'>
             <h1>Order Summary</h1>
             <div id='checkout-detail'>
@@ -11,7 +20,7 @@ function Checkout({cart}){
             <p>Total ${total}</p>
             </div>
         </div>
-    )
+    )}
 }
 
 export default Checkout;
